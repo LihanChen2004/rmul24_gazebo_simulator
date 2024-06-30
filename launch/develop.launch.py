b54509065e13a4a2f580fd58a80200a104a56e8a
LihanChen2004 <757003373@qq.com>
2024-06-30 22:34:49 +0800

    ✨ feat: 支持机器人命名空间

--- launch/develop.launch.py
+++ launch/develop.launch.py
@@ -1,17 +1,3 @@
-# Copyright 2021 RoboMaster-OSS
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -19,7 +5,6 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 from nav2_common.launch import ReplaceString
 
 from xmacro.xmacro4sdf import XMLMacro4sdf
@@ -27,9 +12,6 @@ from sdformat_tools.urdf_generator import UrdfGenerator
 
 def generate_launch_description():
     ld = LaunchDescription()
-    position_x = LaunchConfiguration("position_x")
-    position_y = LaunchConfiguration("position_y")
-    orientation_yaw = LaunchConfiguration("orientation_yaw")
 
     pkg_rmul24_gazebo_simulator = get_package_share_directory('rmul24_gazebo_simulator')
     world_sdf_path = os.path.join(pkg_rmul24_gazebo_simulator, 'resource', 'worlds', 'rmul24_world.sdf')
@@ -37,6 +19,15 @@ def generate_launch_description():
     robot_config = os.path.join(pkg_rmul24_gazebo_simulator, 'config', 'base_params.yaml')
     bridge_config = os.path.join(pkg_rmul24_gazebo_simulator, 'config', 'ros_gz_bridge.yaml')
     robot_sdf_path=os.path.join(pkg_rmul24_gazebo_simulator, 'resource', 'xmacro', 'rmul24_sentry_robot.sdf')
+
+    robots = [
+    {'name': 'red_standard_robot1', 'x_pose': '4.3', 'y_pose': '3.35', 'z_pose': '1.2', 'yaw': '0.0'},
+    # {'name': 'blue_standard_robot1', 'x_pose': '10.0', 'y_pose': '4.5', 'z_pose': '1.2', 'yaw': '3.14'},
+    # ...
+    ]
+
+    robot_macro = XMLMacro4sdf()
+    robot_macro.set_xml_file(robot_xmacro_path)
 
     urdf_generator = UrdfGenerator()
     urdf_generator.parse_from_sdf_file(robot_sdf_path)
@@ -75,42 +66,39 @@ def generate_launch_description():
         }.items()
     )
 
-    robot_names=["red_standard_robot1"]
-
-    # Spawn robot
-    robot_macro = XMLMacro4sdf()
-    robot_macro.set_xml_file(robot_xmacro_path)
-    robot_macro.generate({'global_initial_color': 'red'})
-    robot_xml = robot_macro.to_string()
-
-    gz_spawn_entity = Node(
-        package="ros_gz_sim",
-        executable="create",
-        arguments=[
-            '-string', robot_xml,
-            "-name", robot_names[0],
-            "-allow_renaming", "true",
-            "-x", position_x,
-            "-y", position_y,
-            "-z", "1.2",
-            "-Y", orientation_yaw
-        ]
-    )
-
     # robot base for each robot
-    for robot_name in robot_names:
-        params_file = ReplaceString(
+    for robot in robots:
+
+        robot_macro.generate({'global_initial_color': 'blue'})
+        robot_xml = robot_macro.to_string()
+        spawn_robot = Node(
+            package="ros_gz_sim",
+            executable="create",
+            arguments=[
+                '-string', robot_xml,
+                "-name", robot['name'],
+                "-allow_renaming", "true",
+                "-x", robot['x_pose'],
+                "-y", robot['y_pose'],
+                "-z", robot['z_pose'],
+                "-Y", robot['yaw'],
+            ]
+        )
+        ld.add_action(spawn_robot)
+
+        # replace the robot name in the bridge config file
+        aft_replace_ros_bridge_params = ReplaceString(
             source_file=bridge_config,
-            replacements={'<robot_name>': robot_name},
+            replacements={'<robot_name>': robot['name']},
         )
 
         robot_base = Node(
             package='rmoss_gz_base',
             executable='rmua19_robot_base',
-            namespace=robot_name,
+            namespace=robot['name'],
             parameters=[
                 robot_config,
-                {'robot_name': robot_name},
+                {'robot_name': robot['name']},
             ],
             output='screen')
         ld.add_action(robot_base)
@@ -119,19 +107,24 @@ def generate_launch_description():
             package="robot_state_publisher",
             executable="robot_state_publisher",
             name="robot_state_publisher",
-            parameters=[{'robot_description': robot_urdf_xml}],
+            parameters=[{
+                'robot_description': robot_urdf_xml,
+            }],
             remappings=[
-                ('/joint_states', f'{robot_name}/joint_state'),
+                ('/robot_description', f"{robot['name']}/robot_description"),
+                ('/joint_states', f"{robot['name']}/joint_state"),
+                ('/tf', f"{robot['name']}/tf"),
+                ('/tf_static', f"{robot['name']}/tf_static"),
             ]
         )
-        ld.add_action(robot_state_publisher) # it will publish tf and tf_static, but without namespace
+        ld.add_action(robot_state_publisher)
 
         robot_ign_bridge = Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
-            namespace=robot_name,
+            namespace=robot['name'],
             parameters=[
-                {'config_file': params_file}
+                {'config_file': aft_replace_ros_bridge_params}
             ],
         )
         ld.add_action(robot_ign_bridge)
@@ -142,6 +135,5 @@ def generate_launch_description():
     ld.add_action(declare_position_y_cmd)
     ld.add_action(declare_orientation_yaw_cmd)
     ld.add_action(gazebo)
-    ld.add_action(gz_spawn_entity)
 
     return ld
