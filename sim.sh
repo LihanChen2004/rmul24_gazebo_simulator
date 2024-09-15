03bdab26de4fef17bab0dab552f36accfa473dcd
LihanChen2004 <757003373@qq.com>
2024-09-15 10:42:30 +0800

    🎈 perf: Add useful script

---
+++ sim.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# Start ign simulation
+source install/setup.sh
+
+ros2 launch rmul24_gazebo_simulator develop.launch.py
---
+++ web_referee.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# Start referee_web
+source install/setup.sh
+
+python3 src/rmul24_gazebo_simulator/scripts/referee_web/main.py
